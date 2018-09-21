7de8171403c8b8a188b93f66ff1352af9c973d20
Josh <josh@Joshs-MacBook-Pro-4.local>
2018-09-20 20:18:01 -0400

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'really cool article',
+            'author': 'JD',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'really cool article 2',
+            'author': 'Bob',
+            'create_date':'04-26-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'really cool article 3',
+            'author': 'Sally',
+            'create_date':'04-27-2017'
+        }
+    ]
+    return articles
